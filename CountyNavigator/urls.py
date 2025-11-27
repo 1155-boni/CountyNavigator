b80a4d47c8c482cc415ebb0df6a51ed56c840918
Boniface Masila <dietboni@gmail.com>
2025-11-27 16:14:08 +0300

    Add login redirect to sacco_users login

--- CountyNavigator/urls.py
+++ CountyNavigator/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', RedirectView.as_view(url='/sacco_users/login/', permanent=False)),
     path('sacco_users/', include('sacco_users.urls')),
 ]
 
